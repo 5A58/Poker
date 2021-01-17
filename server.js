ef0a42f6718150e3d624914e83b10948d53e7ef8
Hex <28637164+486578@users.noreply.github.com>
2021-01-16 23:17:26 -0800

    Add Babel for JSX support

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Express config
 const app = express();
-app.use(express.static(join(__dirname, 'client')));
-app.use('/dist', express.static(join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist/client')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back the index.html file
